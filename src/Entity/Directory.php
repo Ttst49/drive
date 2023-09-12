0fd269b03e0d5b5be94c720997cca4332b028795
Thibaut Stachnick <thibautstachnick@gmail.com>
2023-09-12 09:48:26 +0200

    add the first entities and ui on the website

--- src/Entity/Directory.php
+++ src/Entity/Directory.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\DirectoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -20,9 +21,16 @@ class Directory
     #[ORM\ManyToMany(targetEntity: File::class, inversedBy: 'directories')]
     private Collection $files;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
     public function __construct()
     {
         $this->files = new ArrayCollection();
+        $this->name = "Nouveau dossier";
     }
 
 
@@ -51,6 +59,30 @@ class Directory
     public function removeFile(File $file): static
     {
         $this->files->removeElement($file);
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
 
         return $this;
     }
